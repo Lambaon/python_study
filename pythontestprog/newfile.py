ebfa7f5b422f864ecb4388ab5bf5d25f96092fa5
Lambaon <vborkonyuk@gmail.com>
2025-10-06 21:17:34 +0200

    Temperatura

---
+++ pythontestprog/newfile.py
@@ -0,0 +1,19 @@
+def main():
+    print("You have to enter a choice F-Fahrenheit, K-Kelvin")
+
+    while True:
+        choice = input("Enter your choice: ")
+        celsius = float(input("Enter temperature in Celsius: "))
+        if choice == "F" or choice == "Fahrenheit" or choice == "f":
+            fahrenheit = (celsius * 9 / 5) + 32
+            print("Temperature in Fahrenheit: {:.2f}".format(fahrenheit))
+        elif choice == "K" or choice == "Kelvin" or choice == "k":
+            kelvin = celsius + 273.15
+            print("Temperature in Kelvin: {:.2f}".format(kelvin))
+        elif choice == " ":
+            print("invalid ty")
+        else:
+            print("Invalid answer. Please type like in choice.")
+
+
+main()
