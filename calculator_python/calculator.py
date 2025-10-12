6016e98df4dd3469e320769399a7de2ea4d81ab8
Lambaon <vborkonyuk@gmail.com>
2025-10-12 20:45:07 +0200

    Upgrade_calculator

--- calculator_python/calculator.py
+++ calculator_python/calculator.py
@@ -1,23 +1,42 @@
+from decimal import Decimal, InvalidOperation
+
+
+def decimal_to_binary(value):
+    value = value.strip()
+    try:
+        if value.endswith('%'):
+            num = Decimal(value[:-1]) / 100
+        else:
+            num = Decimal(value)
+        return num
+    except InvalidOperation:
+        print("❌ Invalid number format:", value)
+        return Decimal(0)
+
+
 def calculator():
     print("Welcome to the calculator")
-    print("You have to enter a choice +,-,*,/")
-    choice = input("Enter your choice: ")
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
+    num1 = decimal_to_binary(input("Enter the first number: "))
+    num2 = decimal_to_binary(input("Enter the second number: "))
 
-    if choice == "+":
-        print(num1 + num2)
-    elif choice == "-":
-        print(num1 - num2)
-    elif choice == "*":
-        print(num1 * num2)
-    elif choice == "/":
-        if num2 != 0:
-            print("Result: ", num1 / num2)
+    while True:
+        print("You have to enter a choice +,-,*,/")
+        choice = input("Enter your choice: ")
+        if choice == "+":
+            print(num1 + num2)
+        elif choice == "-":
+            print(num1 - num2)
+        elif choice == "*":
+            print(num1 * num2)
+        elif choice == "/":
+            if num2 != 0:
+                print("Result: ", num1 / num2)
+            else:
+                print("No you can't divide by zero")
+                return ()
         else:
-            print("No you can't divide by zero")
-    else:
-        print("Invalid choice")
+            print("Invalid choice-_-")
+        exit()
 
 
 calculator()
